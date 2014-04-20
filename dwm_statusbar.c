b0dc1ac15e833bb1151c8413d1d0bdb4fb5b4ce8
d4ryus <w.wackerbauer@yahoo.de>
2014-04-20 21:24:16 +0200

    changed error handling, will now display error and retry instead of stopping thread"

--- dwm_statusbar.c
+++ dwm_statusbar.c
@@ -91,7 +91,9 @@ void *update_ram(void * val)
         fd = fopen(RAM, "r");
         if(fd == NULL)
         {
-            error("Error opening meminfo");
+            sprintf(displayed_ram, " coudln't read file :( ");
+            sleep(ram_sleep);
+            continue;
         }
         fscanf(fd, "%s %d %s", &buffer, &ram_total,  &buffer);
         fscanf(fd, "%s %d %s", &buffer, &ram_free, &buffer);
@@ -209,23 +211,43 @@ void *update_sound(void * val)
     while(1)
     {
         if (snd_mixer_open(&h_mixer, 1) < 0)
-            error("Alsa_Mixer open error");
+        {
+            sprintf(displayed_sound, "error on snd_mixer_open");
+            sleep(sound_sleep);
+            continue;
+        }
 
         if (snd_mixer_attach(h_mixer, "default") < 0)
-            error("Alsa_Mixer attach error");
+        {
+            sprintf(displayed_sound, "error on snd_mixer_attach");
+            sleep(sound_sleep);
+            continue;
+        }
 
         if (snd_mixer_selem_register(h_mixer, NULL, NULL) < 0)
-            error("Alsa_Mixer simple element register error");
+        {
+            sprintf(displayed_sound, "error on snd_mixer_selem_register");
+            sleep(sound_sleep);
+            continue;
+        }
 
         if (snd_mixer_load(h_mixer) < 0)
-            error("Alsa_Mixer load error");
+        {
+            sprintf(displayed_sound, "error on snd_mixer_load");
+            sleep(sound_sleep);
+            continue;
+        }
 
         snd_mixer_selem_id_alloca(&sid);
         snd_mixer_selem_id_set_index(sid, 0);
         snd_mixer_selem_id_set_name(sid, "Master");
 
         if ((elem = snd_mixer_find_selem(h_mixer, sid)) == NULL)
-            error("Cannot find simple element");
+        {
+            sprintf(displayed_sound, "error on snd_mixer_find_selem");
+            sleep(sound_sleep);
+            continue;
+        }
 
         snd_mixer_selem_get_playback_volume(elem, SND_MIXER_SCHN_FRONT_LEFT, &vol);
         snd_mixer_selem_get_playback_volume_range(elem, &vol_min, &vol_max);
